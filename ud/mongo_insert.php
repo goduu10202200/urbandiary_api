7398896bac5631946a8adfa33fc300160b8c00d5
goduu10202200 <goduu10202200@gmail.com>
2018-11-12 00:27:13 +0800

    1. 測試註冊資料連接API

---
+++ ud/mongo_insert.php
@@ -0,0 +1,27 @@
+<?php
+ include 'DBConnection_mongo.php';
+ $raw_post_data = json_decode(file_get_contents('php://input'), true);
+
+ $collection = 'member';
+ $today = date("Y-m-d H:i:s");
+ $name =  $raw_post_data['name'];
+ $username = $raw_post_data['account'];
+ $password =  $raw_post_data['password'];
+ 
+
+// 連線資料庫
+$manager = new MongoDB\Driver\Manager("mongodb://".$dbhost);
+
+// 插入資料，BulkWrite是批量插入
+$bulk = new MongoDB\Driver\BulkWrite;
+
+// 新增資料
+$bulk->insert([
+    'username' => $username,
+    'name' =>$name,
+    'password' => $password,
+    'created_at' => $today,
+]);
+
+// 執行BulkWrite
+$manager->executeBulkWrite($dbname.'.'.$collection, $bulk);

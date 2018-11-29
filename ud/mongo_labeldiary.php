9bbc76e50e1ca8d1a4695c1a080df457fdf7eb48
sandy010924 <sandy010924@gmail.com>
2018-11-29 14:58:56 +0800

    1. [日記]標籤設置日期與勾選是否完成限制(是今日且已完成的待辦事項才會出現) 2. 勾選待辦事項評分情緒功能，並且在標籤內出現心情描述字句

--- ud/mongo_labeldiary.php
+++ ud/mongo_labeldiary.php
@@ -7,6 +7,7 @@
 
  // 設定變數
  $response_data[] = "";
+ $today = date("Y-m-d");
  $type = $raw_post_data['type'];
   
 
@@ -14,7 +15,7 @@
 $manager = new MongoDB\Driver\Manager("mongodb://".$dbhost);
 
 // 查詢條件
-$filter = ['type' =>  $type];          // 欄位名及匹配條件
+$filter = ['type' =>  $type, 'date' => $today, 'status' =>  1];          // 欄位名及匹配條件
 
 // 查詢資料
 $query = new MongoDB\Driver\Query($filter);
@@ -28,13 +29,47 @@ foreach ($cursor as $key =>  $document) {
     $response_data[] = array(
         "title"         =>   $decode_data['title'],
         "location"   =>   $decode_data['location'],
+        "mood"   =>   $decode_data['mood'],
     );
 }
+
+$mood_1 = array("好累喔！", "有點煩。");
+$mood_2 = array("有點累。");
+$mood_3 = array("感覺還行。", "覺得還可以。");
+$mood_4 = array("感覺不錯！", "還蠻喜歡的(๑´ڡ`๑)");
+$mood_5 = array("超開心的啦～", "讚喔XD", "水喔");
+
+
 
 // 這個方法很爛不要學
 if (count($response_data) > 1) {
     $decode_data = json_decode(json_encode($response_data[(count($response_data)-1)]), true);
-    echo("在".$decode_data['location']."，".$decode_data['title']);
-} else {
+    switch($decode_data['mood']){
+        case 1:
+            $mood = $mood_1[rand(0,count($mood_1)-1)];
+        break;
+        case 2:
+            $mood = $mood_2[rand(0,count($mood_2)-1)];
+        break;
+        case 3:
+            $mood = $mood_3[rand(0,count($mood_3)-1)];
+        break;
+        case 4:
+            $mood = $mood_4[rand(0,count($mood_4)-1)];
+        break;
+        case 5:
+            $mood = $mood_5[rand(0,count($mood_5)-1)];
+        break;
+        default:
+            $mood = "";
+
+    }
+    if($decode_data['location'] === "地點"){
+        echo($decode_data['title']."，".$mood);
+    }
+    else{
+        echo("在".$decode_data['location']."，".$decode_data['title']."，".$mood);
+    }
+    } else {
     echo "";
 }
---
+++ ud/mongo_listmood.php
@@ -0,0 +1,31 @@
+<?php
+ include 'DBConnection_mongo.php';
+ $raw_post_data = json_decode(file_get_contents('php://input'), true);
+
+ // 資料表
+ $collection = 'scheduled';
+
+ // 設定變數
+ $created_at =  $raw_post_data['created_at'];
+ $mood =  $raw_post_data['mood'];
+
+// 連線資料庫
+$manager = new MongoDB\Driver\Manager("mongodb://".$dbhost);
+
+// 插入資料，BulkWrite是批量插入
+$bulk = new MongoDB\Driver\BulkWrite;
+
+//更新資料
+$bulk->update(
+    ['created_at' => $created_at],
+    ['$set' => ['mood' => $mood]]
+);
+
+// 執行BulkWrite
+$manager->executeBulkWrite($dbname.'.'.$collection, $bulk);
+//判斷是否執行成功
+// if ($conn->query($sql) === true) {
+//     echo "Check successfully";
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+// }

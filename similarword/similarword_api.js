cebfc34f57c1caa3c4dc34e26b58321a94c800a8
goduu10202200 <goduu10202200@gmail.com>
2018-12-03 22:58:33 +0800

    1. 修改相近詞API function(node.js) 2. 修改相近詞API (python)

--- similarword/similarword_api.js
+++ similarword/similarword_api.js
@@ -1,9 +1,9 @@
 var http = require("http");
 var async = require("async");
 var axios = require("axios");
+// Import MongoDB
 var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/";
-//cheerio = require("cheerio");
 
 // Imports the Google Cloud client library
 const { Translate } = require("@google-cloud/translate");
@@ -26,15 +26,13 @@ var server = http.createServer(function(req, res) {
   if (req.method == "POST") {
     var jsonString = "";
     var jsonparse_jsonstring = "";
+
     req.on("data", function(data) {
       jsonString += data;
     });
-
     req.on("end", function() {
       jsonparse_jsonstring = JSON.parse(jsonString);
-      // similarword(JSON.parse(jsonparse_jsonstring["word"]));
-      translateword(JSON.parse(jsonparse_jsonstring["word"]));
-      //console.log(jsonparse_jsonstring);
+      translate_word_array(JSON.parse(jsonparse_jsonstring["word"]));
     });
   }
 });
@@ -43,40 +41,6 @@ server.listen(6000); //3 - 進入此網站的監聽 port, 就是 localhost:xxxx 
 
 console.log("Node.js web server at port 6000 is running..");
 
-// 測試翻譯funciton
-async function test_translate(array_word) {
-  var final_word = [];
-  // Translates some text into Russian
-  for (var i = 0; i < array_word.length; i++) {
-    // The text to translate
-    const text = array_word[i];
-    // await a_translate(text)
-    //   .then(gg => {
-    //     final_word.push(gg);
-    //   })
-    //   .catch(console.error);
-    const target = "zh-tw";
-    var res = await translate.translate(array_word[i], "zh-tw");
-    final_word.push(res[0]);
-  }
-  // array_word.forEach(
-  //   element => {
-  //     var res = translate.translate("余佩珊", "zh-tw");
-  //     final_word.push(res[0]);
-  //   },
-  //   () => {
-  //     return final_word;
-  //   }
-  // );
-  return final_word;
-}
-async function a_translate(text) {
-  const target = "zh-tw";
-  var res = await translate.translate(text, target);
-  var gg = res[0];
-  return gg;
-}
-
 // 相近詞function
 function similarword(word) {
   axios
@@ -84,51 +48,25 @@ function similarword(word) {
       word: word
     })
     .then(function(response) {
+      // Get json data
       var json_word = JSON.parse(JSON.stringify(response.data));
       var json_analysis_word = JSON.parse(
         JSON.stringify(response.data["analysis_word"])
       );
       replace_analysis_word = json_analysis_word.replace(/'/g, '"');
       array_analysis_word = JSON.parse(replace_analysis_word);
+
       // 將Tag word push 到 array
       array_analysis_word.push(json_word["word"]);
 
       //Start translate words
-      test_translate(array_analysis_word)
+      translate_word(array_analysis_word)
         .then(final_word => {
           // console.log(final_word);
           if (final_word != "Promise { <pending> }") {
-            MongoClient.connect(
-              url,
-              function(err, db) {
-                if (err) throw err;
-                var dbo = db.db("ud");
-                var tag_word = "";
-                // 抓取Tag 文字
-                if (final_word.length == 1) {
-                  tag_word = final_word[0];
-                  console.log("1111" + tag_word);
-                } else {
-                  tag_word = final_word[final_word.length - 1];
-                  console.log("final" + tag_word);
-                }
-                // 設定新增資料
-                var myobj = [
-                  {
-                    tag: tag_word,
-                    similarword: final_word
-                  }
-                ];
-                //新增資料;
-                dbo.collection("test").insertMany(myobj, function(err, res) {
-                  if (err) throw err;
-                  console.log("插入的數量 : " + res.insertedCount);
-                  db.close();
-                });
-              }
-            );
+            // Insert data to MongoDB
+            insertDB(final_word);
           }
-          return final_word;
         })
         .catch(console.error);
     })
@@ -138,14 +76,13 @@ function similarword(word) {
 }
 
 // 翻譯function
-function translateword(tr_word) {
+function translate_word_array(tr_word) {
   // The target language
   const target = "zh-cn";
   // Translates some text into Russian
   for (var i = 0; i < tr_word.length; i++) {
     // The text to translate
     const text = tr_word[i];
-    // similarword(text);
     translate
       .translate(text, target)
       .then(results => {
@@ -156,4 +93,46 @@ function translateword(tr_word) {
         console.error("ERROR:", err);
       });
   }
+}
+
+// 新增資料function
+function insertDB(final_word) {
+  MongoClient.connect(
+    url,
+    function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("ud");
+      var tag_word = "";
+      // 抓取Tag 文字
+      if (final_word.length == 1) {
+        tag_word = final_word[0];
+      } else {
+        tag_word = final_word[final_word.length - 1];
+      }
+      // 設定新增資料
+      var myobj = [
+        {
+          tag: tag_word,
+          similarword: final_word
+        }
+      ];
+      //新增資料;
+      dbo.collection("similarword").insertMany(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("插入的數量 : " + res.insertedCount);
+        db.close();
+      });
+    }
+  );
+}
+// 翻譯funciton
+async function translate_word(array_word) {
+  var final_word = [];
+  // Translates some text into Russian
+  for (var i = 0; i < array_word.length; i++) {
+    // Google translate API
+    var res = await translate.translate(array_word[i], "zh-tw");
+    final_word.push(res[0]);
+  }
+  return final_word;
 }
--- similarword/similarword_api.py
+++ similarword/similarword_api.py
@@ -1,14 +1,7 @@
 import synonyms
-#from py_translator import Translator
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import socketserver
 import json
-import cgi
-from io import BytesIO
-import simplejson
-
-
-
 
 # Web Server 設定
 class Handler(BaseHTTPRequestHandler): 
@@ -24,40 +17,21 @@ class Handler(BaseHTTPRequestHandler):
         
         # 設定Text Array
         array_text = data['word']
-        array_trans_text = []
         array_analysis_text = []
-        
-        #self.wfile.write(b"<html><head><title>Title goes here.</title></head><body><div class='dd'>")
-        
-        #for index in range(len(array_text)):
-            #trans_text = Translator().translate(text = array_text[index], dest='zh-cn').text
-            #array_trans_text.append(trans_text)
 
         # 分析相近詞 => 存入到陣列
         array_analysis_text.append(synonyms.nearby(array_text))
-
-        # 設定變數
-        byte1 = b'<p>'
-        byte2 = b'</p>'
 
         response = {
             'word':str(array_text),
             'analysis_word':str(array_analysis_text[0][0])
         }
 
-            
-        word = json.dumps(str(array_text) + ": " + str(array_analysis_text[0][0])).encode()
-        word2 = json.dumps(response).encode()
+        word = json.dumps(response).encode()
         # 寫入到Web
-        #self.wfile.write(byte1)
         self.do_HEAD()
-        self.wfile.write(word2)
-        #self.wfile.write(byte2)
-            
-        
-        #self.wfile.write(b"</div></body></html>")         
+        self.wfile.write(word)
         return
-
 print('Server listening on port 8000...')
 # 執行Web Server
 httpd = socketserver.TCPServer(('', 8000), Handler)

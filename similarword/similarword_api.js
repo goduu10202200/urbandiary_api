97aa849af31d061a53dc20773377ac5214dda0c0
goduu10202200 <goduu10202200@gmail.com>
2018-12-03 16:38:29 +0800

    1. 相近詞修改 1. 相近詞存進資料庫

--- similarword/similarword_api.js
+++ similarword/similarword_api.js
@@ -46,89 +46,91 @@ console.log("Node.js web server at port 6000 is running..");
 // 測試翻譯funciton
 async function test_translate(array_word) {
   var final_word = [];
-  // The target language
-  const target = "zh-cn";
   // Translates some text into Russian
   for (var i = 0; i < array_word.length; i++) {
     // The text to translate
     const text = array_word[i];
-    // Push array
-    var val = await a_translate(text);
-    final_word.push(val);
+    // await a_translate(text)
+    //   .then(gg => {
+    //     final_word.push(gg);
+    //   })
+    //   .catch(console.error);
+    const target = "zh-tw";
+    var res = await translate.translate(array_word[i], "zh-tw");
+    final_word.push(res[0]);
   }
+  // array_word.forEach(
+  //   element => {
+  //     var res = translate.translate("余佩珊", "zh-tw");
+  //     final_word.push(res[0]);
+  //   },
+  //   () => {
+  //     return final_word;
+  //   }
+  // );
   return final_word;
 }
-
 async function a_translate(text) {
-  var k = "";
-  translate
-    .translate(text, target)
-    .then(results => {
-      k = results[0];
-    })
-    .catch(err => {
-      console.error("ERROR:", err);
-    });
-  return k;
+  const target = "zh-tw";
+  var res = await translate.translate(text, target);
+  var gg = res[0];
+  return gg;
 }
 
 // 相近詞function
 function similarword(word) {
-  //translateword(word);
-  var array_t = [];
-
   axios
     .post("http://localhost:8000/", {
       word: word
     })
     .then(function(response) {
-      // // 取得網頁所有資料
-      // let $ = cheerio.load(response.data);
-      // // 取得 class dd 之後的資料
-      // const table_tr = $(".dd");
-      // // 取得P標籤
-      // const test = table_tr.eq(0).find("p");
-      // // 顯示所有P標籤的資料
-      // for (var i = 0; i < test.length; i++) {
-      //   console.log(JSON.parse(test.eq(i).text()));
-      //   //return JSON.parse(test.eq(i).text());
-      // }
       var json_word = JSON.parse(JSON.stringify(response.data));
       var json_analysis_word = JSON.parse(
         JSON.stringify(response.data["analysis_word"])
       );
       replace_analysis_word = json_analysis_word.replace(/'/g, '"');
       array_analysis_word = JSON.parse(replace_analysis_word);
+      // 將Tag word push 到 array
+      array_analysis_word.push(json_word["word"]);
 
-      // console.log("word" + json_word["word"]);
-      // for (var i = 0; i < array_analysis_word.length; i++) {
-      //   console.log(array_analysis_word[i]);
-      // }
-
-      console.log(test_translate(array_analysis_word));
-
-      // MongoClient.connect(
-      //   url,
-      //   function(err, db) {
-      //     if (err) throw err;
-      //     var dbo = db.db("ud");
-
-      //     // 設定新增資料
-      //     var myobj = [
-      //       {
-      //         tag: split_json_word[0],
-      //         similarword: split_json_word[1]
-      //       }
-      //     ];
-
-      //     //新增資料;
-      //     dbo.collection("test").insertMany(myobj, function(err, res) {
-      //       if (err) throw err;
-      //       console.log("插入的數量 : " + res.insertedCount);
-      //       db.close();
-      //     });
-      //   }
-      // );
+      //Start translate words
+      test_translate(array_analysis_word)
+        .then(final_word => {
+          // console.log(final_word);
+          if (final_word != "Promise { <pending> }") {
+            MongoClient.connect(
+              url,
+              function(err, db) {
+                if (err) throw err;
+                var dbo = db.db("ud");
+                var tag_word = "";
+                // 抓取Tag 文字
+                if (final_word.length == 1) {
+                  tag_word = final_word[0];
+                  console.log("1111" + tag_word);
+                } else {
+                  tag_word = final_word[final_word.length - 1];
+                  console.log("final" + tag_word);
+                }
+                // 設定新增資料
+                var myobj = [
+                  {
+                    tag: tag_word,
+                    similarword: final_word
+                  }
+                ];
+                //新增資料;
+                dbo.collection("test").insertMany(myobj, function(err, res) {
+                  if (err) throw err;
+                  console.log("插入的數量 : " + res.insertedCount);
+                  db.close();
+                });
+              }
+            );
+          }
+          return final_word;
+        })
+        .catch(console.error);
     })
     .catch(function(e) {
       console.log(e);
@@ -143,17 +145,15 @@ function translateword(tr_word) {
   for (var i = 0; i < tr_word.length; i++) {
     // The text to translate
     const text = tr_word[i];
-    similarword(text);
-    // translate
-    //   .translate(text, target)
-    //   .then(results => {
-    //     // const translation = results[0];
-    //     // console.log("Text:" + text);
-    //     // console.log(`Translation: ${translation}`);
-    //     similarword(results[0]);
-    //   })
-    //   .catch(err => {
-    //     console.error("ERROR:", err);
-    //   });
+    // similarword(text);
+    translate
+      .translate(text, target)
+      .then(results => {
+        // 連接相近詞API
+        similarword(results[0]);
+      })
+      .catch(err => {
+        console.error("ERROR:", err);
+      });
   }
 }

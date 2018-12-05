2ec77dd7e67f0ba3e25015f94aea9d5aecda0793
sandy010924 <sandy010924@gmail.com>
2018-12-06 00:23:26 +0800

    1. 日記上傳照片及顯示照片功能

---
+++ ud/mongo_viewphoto.php
@@ -0,0 +1,40 @@
+<?php
+ include 'DBConnection_mongo.php';
+ $raw_post_data = json_decode(file_get_contents('php://input'), true);
+
+  // 資料表
+ $collection = 'uploadfile';
+
+ // 設定變數
+$today = date("Y-m-d");
+ 
+// 連線資料庫
+$manager = new MongoDB\Driver\Manager("mongodb://".$dbhost);
+
+// 查詢條件
+$filter = ['created_at' =>  $today];          // 欄位名及匹配條件
+$options = [
+    'sort'       => ['kind'=>-1],
+];
+
+// 查詢資料
+$query = new MongoDB\Driver\Query($filter);
+$cursor = $manager->executeQuery($dbname.'.'.$collection, $query);
+
+// 判斷是否有資料
+$i = 0;
+foreach ($cursor as $key =>  $document) {
+    $decode_data = json_decode(json_encode($document), true);
+    $response_data[] = array(
+        "name" =>   $decode_data['name']
+    );
+    $i += ($key+1);
+}
+
+
+    echo json_encode($response_data);
+// if( $i > 0 ){
+// }
+// else{
+//     echo "No data";
+// }

5aa24bb2ec107a8790637cee68b46e27f7dfb5a7
goduu10202200 <goduu10202200@gmail.com>
2018-12-10 23:01:33 +0800

    1. 修改uploadphoto 連接 相片分析API 2. mongo_diary的username 更改為e-mail

--- ud/mongo_diary.php
+++ ud/mongo_diary.php
@@ -8,7 +8,7 @@
  // 設定變數
 $today = date("Y-m-d");
 $content =  $raw_post_data['content'];
-    
+$imagefilename =  $raw_post_data['imagefilename'];
 
 // 連線資料庫
 $manager = new MongoDB\Driver\Manager("mongodb://".$dbhost);
@@ -18,9 +18,10 @@ $bulk = new MongoDB\Driver\BulkWrite;
 
 // 新增資料
 $bulk->insert([
-    'username'      => 1,
+    'username'      => "mau123@gmail.com",
     'content'       => $content,
     'date'          => $today,
+    'imagefilename' => $imagefilename,
     'jieba_check'   => '0'
 ]);
 
@@ -28,6 +29,8 @@ try {
     // 執行BulkWrite
     $manager->executeBulkWrite($dbname.'.'.$collection, $bulk);
     header('Location: http://172.20.10.3:5000/jieba');
+    // header('Location: http://192.168.1.104:5000/jieba');
+    
     exit;
 } catch (MongoCursorException $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
--- ud/mongo_uploadphoto.php
+++ ud/mongo_uploadphoto.php
@@ -3,6 +3,7 @@
  $raw_post_data = json_decode(file_get_contents('php://input'), true);
 
 $uploaddir = "../../assets/uploadfile/images/";
+$uploadURL = "http://140.135.114.124:90/ud/assets/uploadfile/images/";
 $uploadfile = $uploaddir.basename($_FILES['file']['name']);
 
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $uploadfile)) {
@@ -29,7 +30,57 @@ if (move_uploaded_file($_FILES["file"]["tmp_name"], $uploadfile)) {
     // 執行BulkWrite
     $manager->executeBulkWrite($dbname.'.'.$collection, $bulk);
 
-    echo "File is valid, and was successfully uploaded.\n";
+
+    // 這邊串上傳後取得的圖片路徑跟檔名
+    $image_url = $uploadURL.$_FILES['file']['name'];
+    
+    $api_credentials = array(
+         'key' => 'acc_1cb310f3992c397',
+         'secret' => 'b0af6bd957c3159b0e67842523174a32'
+     );
+ 
+    // 參數 ['語系', '閥值']
+    $lang = 'zh_cht';
+    $thresold = '40';
+ 
+    // 開始執行 curl
+    $ch = curl_init();
+ 
+    curl_setopt($ch, CURLOPT_URL, 'https://api.imagga.com/v2/tags?image_url='.$image_url.'&language='.$lang.'&threshold='.$thresold);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_USERPWD, $api_credentials['key'].':'.$api_credentials['secret']);
+ 
+    $response = curl_exec($ch);
+    curl_close($ch);
+ 
+    $json_response = json_decode($response);
+     
+    echo json_encode($json_response);
+
+
+    // 資料表
+    $collection = 'imagga';
+
+    // 連線資料庫
+    $manager = new MongoDB\Driver\Manager("mongodb://".$dbhost);
+
+    // 插入資料，BulkWrite是批量插入
+    $bulk = new MongoDB\Driver\BulkWrite;
+
+    // 新增資料
+    $bulk->insert([
+        'filename'      => $_FILES["file"]["name"],
+        'resault'       => $json_response,
+    ]);
+
+    try {
+        // 執行BulkWrite
+        $manager->executeBulkWrite($dbname.'.'.$collection, $bulk);
+        echo "File is valid, and was successfully uploaded.\n";
+    } catch (MongoCursorException $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    }
 } else {
     echo "Upload failed";
 }
